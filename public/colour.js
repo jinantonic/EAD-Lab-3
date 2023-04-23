9ef0225dd762babc946ef432f36ef200cf7beeac
Jina Park <jinapark119@gmail.com>
2023-04-23 22:13:17 +0100

    here

--- public/colour.js
+++ public/colour.js
@@ -2,9 +2,19 @@
     var currentPage = 0; // The current page of data being displayed
     
     $(document).ready(function() { // When the document is ready
+        $("#ColourIdNav").on("keyup", function(event) {
+            if (event.keyCode === 13) { // If Enter key is pressed
+              event.preventDefault();
+              const colourId = $(this).val();
+              $("#ColourId").val(colourId);
+              $("#btn1").click(); // Trigger button 1 click event
+            }
+          });
+
+          
         $("#btn1").click(function() { // When the button 1 is clicked
             const colourId = $('#ColourId').val();
-            console.log(colourId);
+            //console.log(colourId);
             $.get(`/colours/${colourId}`, function(colour) {
                 // Update the display with the colour details
                 $('#hexString').val(colour.hexString);
@@ -13,6 +23,9 @@
                 $('#Name').val(colour.name);
                 $('.colour-sample').css('background-color', colour.hexString);
                 document.cookie = "colourId=" + colourId + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+
+                // update the value of the input box between arrows
+                $('#ColourIdNav').val(colourId);
             }).fail(function() {
                 // Display an error message if the colour is not found
                 alert('Colour not found');
@@ -25,7 +38,29 @@
         if (currentColor) {
             $("#ColourId").val(currentColor);
             $("#btn1").click();
+            $('#ColourIdNav').val(currentColor); // set the value of the input box between arrows
         }
+
+        const maxColor = 256;
+        $("#btnLeft").click(function() { // When the left arrow button is clicked
+            let colourId = parseInt($('#ColourIdNav').val());
+            if (colourId <= 1) {
+                colourId = maxColor;
+            } else {
+                colourId--;
+            }
+            $('#ColourIdNav').val(colourId);
+        });
+    
+        $("#btnRight").click(function() { // When the right arrow button is clicked
+            let colourId = parseInt($('#ColourIdNav').val());
+            if (colourId >= maxColor) {
+                colourId = 1;
+            } else {
+                colourId++;
+            }
+            $('#ColourIdNav').val(colourId);
+        });
 
         $("#btn2").click(function() { // When the button 2 is clicked
             const hexString = $('#hexString').val();
@@ -94,7 +129,8 @@
             $.ajax({
                 url: `/colours/${colourId}`,
                 type: 'PUT',
-                data: { hexString, rgb, hsl, name },
+                contentType: 'application/json', // Add this line
+                data: JSON.stringify({ hexString, rgb, hsl, name }), // Modify this line
                 success: function(result) {
                     alert(`Colour ${name} modified successfully`);
                 },
@@ -103,11 +139,10 @@
                     alert('Error modifying colour');
                 }
             });
-
-
         }); // End Buttion 4
+        
 
-        $("#btn5").click(function() { // When the button 5 is clicked
+        $("#btn5").click(function() {
             const colourId = $('#ColourId').val();
             $.get(`/colours/${colourId}`, function(colour) {
                 // Update the display with the colour details
@@ -116,40 +151,39 @@
                 $('#HSL').val("HSL(" + colour.hsl.h + ", " + colour.hsl.s + ", " + colour.hsl.l + ")");
                 $('#Name').val(colour.name);
                 $('.colour-sample').css('background-color', colour.hexString);
+                $('body').css('background-color', colour.hexString); // Change the background color of the entire page
                 document.cookie = "colourId=" + colourId + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
             }).fail(function() {
                 // Display an error message if the colour is not found
                 alert('Colour not found');
                 $(location).attr('href','404.html');
             });
-
-
         }); // End Button 5
+        
 
         $("#btn6").click(function() { // When the button 6 is clicked
-    $("#table-content tbody").empty(); // clear the table body first
-    $.get("/colours", function(colours) {
-        $.each(colours, function(index, colour) {
-            const rgbString = "RGB(" + colour.rgb.r + ", " + colour.rgb.g + ", " + colour.rgb.b + ")";
-            const hslString = "HSL(" + colour.hsl.h + ", " + colour.hsl.s + ", " + colour.hsl.l + ")";
-            const row = `<tr>
-                            <td><div class="colour-sample" style="background-color:${colour.hexString}"></div></td>
-                            <td>${colour.colorId}</td>
-                            <td>${colour.colorId}</td>
-                            <td>${colour.name}</td>
-                            <td>${colour.hexString}</td>
-                            <td>${rgbString}</td>
-                            <td>${hslString}</td>
-                            
-                        </tr>`;
-            $("#table-content tbody").append(row);
-        });
-    }).fail(function() {
-        // Display an error message if the colours data is not found
-        alert('Colours data not found');
-        $(location).attr('href','404.html');
-    });
-}); // End Button 6
+            $("#table-content tbody").empty(); // clear the table body first
+            $.get("/colours", function(colours) {
+                $.each(colours, function(index, colour) {
+                    const rgbString = "RGB(" + colour.rgb.r + ", " + colour.rgb.g + ", " + colour.rgb.b + ")";
+                    const hslString = "HSL(" + colour.hsl.h + ", " + colour.hsl.s + ", " + colour.hsl.l + ")";
+                    const row = `<tr>
+                                    <td><div class="colour-sample" style="background-color:${colour.hexString}"></div></td>
+                                    <td>${colour.colorId}</td>
+                                    <td>${colour.name}</td>
+                                    <td>${colour.hexString}</td>
+                                    <td>${rgbString}</td>
+                                    <td>${hslString}</td>
+                                    
+                                </tr>`;
+                    $("#table-content tbody").append(row);
+                });
+            }).fail(function() {
+                // Display an error message if the colours data is not found
+                alert('Colours data not found');
+                $(location).attr('href','404.html');
+            });
+        }); // End Button 6
 
 
 
@@ -162,26 +196,49 @@
             $('.colour-sample').css('background-color', '');
         });
 
-        $("#btn8").click(function() {
-            $.ajax({
-                url: '/reset',
-                type: 'PUT',
-                success: function(result) {
-                    alert('Colors reset successfully');
-                    location.reload(); // Reload the page to display the reset data
-                },
-                error: function(xhr, status, error) {
-                    // Display an error message if the reset fails
-                    alert('Error resetting colors');
-                }
-            });
+        // $("#btn8").click(function() {
+        //     $.ajax({
+        //         url: '/reset',
+        //         type: 'PUT',
+        //         contentType: 'application/json', // Add this line
+        //         success: function(result) {
+        //             alert('Colors reset successfully');
+        //             location.reload(); // Reload the page to display the reset data
+        //         },
+        //         error: function(xhr, status, error) {
+        //             // Display an error message if the reset fails
+        //             alert('Error resetting colors');
+        //         }
+        //     });
+        // });
+        
+        $("#btnLeft").click(function() {
+            let currentColourId = parseInt($('#ColourId').val());
+            if (!isNaN(currentColourId) && currentColourId > 1) {
+                currentColourId--;
+                $('#ColourId').val(currentColourId);
+                $("#btn1").trigger("click");
+            }
+        });
+        
+        $("#btnRight").click(function() {
+            let currentColourId = parseInt($('#ColourId').val());
+            if (!isNaN(currentColourId)) {
+                currentColourId++;
+                $('#ColourId').val(currentColourId);
+                $("#btn1").trigger("click");
+            }
         });
         
 
         function getCookie(name) {
-            const cookieValue = document.cookie.match('(^|[^;]+)\\s*' + name + '\\s*=\\s*([^;]+)');
-            return cookieValue ? cookieValue.pop() : null;
-        }
+            const value = "; " + document.cookie;
+            const parts = value.split("; " + name + "=");
+            if (parts.length === 2) {
+              return parts.pop().split(";").shift();
+            }
+          }
+          
 
 
     }); // End document ready
